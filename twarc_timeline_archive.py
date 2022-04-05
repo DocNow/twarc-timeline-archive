9f35b685ed6bf837d274e9dfe49596af41c92744
Igor Brigadir <igor.brigadir@gmail.com>
2022-04-06 00:55:34 +0100

    add try catch to skip user errors, black format

--- twarc_timeline_archive.py
+++ twarc_timeline_archive.py
@@ -1,15 +1,18 @@
-import re
 import json
-import twarc
 import click
 import pathlib
 import datetime
 
-@click.command('timeline-archive')
-@click.option('--use-search', is_flag=True, default=False,
-    help='Use the search/all API endpoint which is not limited to the last 3200 tweets, but requires Academic Product Track access.') 
-@click.argument('infile', type=click.File('r'), default='-')
-@click.argument('output_dir', type=str, default='timelines')
+
+@click.command("timeline-archive")
+@click.option(
+    "--use-search",
+    is_flag=True,
+    default=False,
+    help="Use the search/all API endpoint which is not limited to the last 3200 tweets, but requires Academic Product Track access.",
+)
+@click.argument("infile", type=click.File("r"), default="-")
+@click.argument("output_dir", type=str, default="timelines")
 @click.pass_obj
 def timeline_archive(T, infile, output_dir, use_search):
     """
@@ -22,29 +25,45 @@ def timeline_archive(T, infile, output_dir, use_search):
 
     for line in infile:
         line = line.strip()
-        usernames = False if re.match(r'^\d+$', line) else True
         since_id = get_max_tweet_id(line, output_dir)
-        click.echo(f'🌟  fetching timeline for {line} since {since_id}')
+        click.echo(f"🌟  fetching timeline for {line} since {since_id}")
 
-        # which api endpoint to use
-        if use_search and since_id:
-            tweets = T.search_all(f'from:{line}', since_id=since_id)
-        elif use_search:
-            tweets = T.search_all(f'from:{line}', 
-                start_time=datetime.datetime(2006, 3, 21, tzinfo=datetime.timezone.utc))
-        else:
-            tweets = T.timeline(line, since_id=since_id)
+        try:
+            # which api endpoint to use
+            if use_search and since_id:
+                tweets = T.search_all(f"from:{line}", since_id=since_id)
+            elif use_search:
+                tweets = T.search_all(
+                    f"from:{line}",
+                    start_time=datetime.datetime(
+                        2006, 3, 21, tzinfo=datetime.timezone.utc
+                    ),
+                )
+            else:
+                tweets = T.timeline(line, since_id=since_id)
 
-        for response in tweets:
-            write_response(line, response, output_dir) 
+            for response in tweets:
+                write_response(line, response, output_dir)
+        except Exception as e:
+            click.echo(
+                click.style(
+                    f"💥  Error fetching timeline for {line} since {since_id}, {e}",
+                    fg="red",
+                ),
+                err=True,
+            )
+
 
 file_handles = {}
+
+
 def write_response(user, response, output_dir):
     fh = file_handles.get(user)
     if not fh:
-        fh = get_json_file(user, output_dir).open('a')
+        fh = get_json_file(user, output_dir).open("a")
         file_handles[user] = fh
     fh.write(json.dumps(response) + "\n")
+
 
 def get_max_tweet_id(user, output_dir):
     """
@@ -53,12 +72,13 @@ def get_max_tweet_id(user, output_dir):
     max_tweet_id = None
     json_file = get_json_file(user, output_dir)
     if json_file.is_file():
-        for line in json_file.open('r'):
+        for line in json_file.open("r"):
             response = json.loads(line)
-            for tweet in response['data']:
-                if max_tweet_id is None or int(tweet['id']) > max_tweet_id:
-                    max_tweet_id = int(tweet['id'])
+            for tweet in response["data"]:
+                if max_tweet_id is None or int(tweet["id"]) > max_tweet_id:
+                    max_tweet_id = int(tweet["id"])
     return max_tweet_id
 
+
 def get_json_file(user, output_dir):
-    return pathlib.Path(output_dir / f'{user}.jsonl')
+    return pathlib.Path(output_dir / f"{user}.jsonl")
